4d3bddd267a2d508cce554adf6b4a6b9e71712b7
Robotoaster <swav.kulinski@gmail.com>
2019-01-28 15:56:50 +0000

    cont

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
@@ -603,7 +603,7 @@ public class CameraPlugin implements MethodCallHandler {
             cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
         captureBuilder.addTarget(pictureImageReader.getSurface());
         captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, getMediaOrientation());
-        captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_MACRO);
+        captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
 
         cameraCaptureSession.capture(
             captureBuilder.build(),
